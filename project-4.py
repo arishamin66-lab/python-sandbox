9b404681cb2e0dacae32a5fed3007328724e9958
arishamin66 <arishamin66@gmail.com>
2026-09-25 11:45:46 +0500

    Fourth mini-project of python

---
+++ project-4.py
@@ -0,0 +1,29 @@
+cart = {
+    "apple": 150,
+    "milk": 250,
+    "bread": 180
+}
+
+total = 0
+expensive = cart["apple"]
+expensive_name = "apple"
+cheapest = cart["milk"]
+cheapest_name = "milk"
+count = 0
+
+for key,value in cart.items():
+    total += value
+    count += 1
+    if value > expensive:
+        expensive = value
+        expensive_name = key
+    if value < cheapest:
+        cheapest = value
+        cheapest_name = key
+    
+print(total)
+if total > 500:
+    print("Total is more than 500")
+    
+print(f"{expensive_name}: {expensive}")
+print(f"{cheapest_name}: {cheapest}")
